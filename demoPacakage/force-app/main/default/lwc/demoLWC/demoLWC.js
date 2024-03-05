d07a8593e219c4e692badaba5a72d01e4c546a7d
vamsi0319 <vamsi.k@bitordertech.com>
2024-03-05 14:58:04 +0530

    my new repo

---
+++ demoPacakage/force-app/main/default/lwc/demoLWC/demoLWC.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+
+export default class DemoLWC extends LightningElement {
+    
+}
---
+++ demoPacakage/force-app/main/default/lwc/firstLWC/firstLWC.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class FirstLWC extends LightningElement {
+
+    name = "Amruth";
+    company = "BOT";
+    designation = "Associate Technical Specialist";
+    salary = "$10000";
+}
